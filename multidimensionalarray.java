8d44e8819e79382d69cd6306521cb75dc465f547
Yellugari Harsha Vardhan Reddy <yellugari63@gmail.com>
2022-07-20 10:21:47 +0530

    hloo

---
+++ multidimensionalarray.java
@@ -0,0 +1,13 @@
+
+public class multidimensionalarray {
+	public static void main(String[] args) {
+
+
+	int[][] a = {
+            {1 ,3 , 5, 6 , 9,  10,  7 }, 
+            {3, 6, 7, 8 , 4 , 6 } };
+      
+      System.out.println("\nLength of row 1: " + a[1].length);
+      }
+
+}
---
+++ singledimensionalarray.java
@@ -0,0 +1,13 @@
+
+public class singledimensionalarray {
+	public static void main(String[] args) {
+
+		//single-dimensional array
+		int b[]= {10,20,30,40,25 ,35, 37};
+		for(int i=0;i<7;i++) {
+		System.out.println("Elements of array b: "+b[i]);
+		}
+
+
+	}
+}
